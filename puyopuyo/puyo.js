93e1cc5eca37e56cc98c81b1393a29d2273603e1
kgsk758 <yhasutha2@gmail.com>
2025-03-11 08:44:25 +0900

    ban scroll

--- puyopuyo/puyo.js
+++ puyopuyo/puyo.js
@@ -1,6 +1,5 @@
 //初期設定
 const canvas = document.getElementById("maincanvas");
-//const ui = document.getElementById("UI");
 const ctx = canvas.getContext("2d");
 const ROWS = 14;
 const COLUMNS = 6;
@@ -10,13 +9,26 @@ console.log(width);
 if(width <= 480){
     canvas.style.width = `${COLUMNS*SIZE}px`;
 canvas.style.height = `${(ROWS-2)*SIZE}px`;
-//ui.style.fontsize = "10px";
 }else{
     SIZE += 3;
 canvas.style.width = `${COLUMNS*(SIZE)}px`;
 canvas.style.height = `${(ROWS-2)*(SIZE)}px`;
-//ui.style.fontsize = "100px";
 }
-/*document.addEventListener("touchmove", (event) => {
-    event.preventDefault(); // スワイプしてもスクロールしない
-}, { passive: false });*/
+//タッチ処理
+let is_touched = false;
+document.addEventListener("touchstart", ()=>{
+    is_touched = true;
+});
+document.addEventListener("touchend", ()=>{
+    is_touched = false;
+});
+
+//ずっと処理
+function mainroop(){
+    console.log(is_touched);
+    requestAnimationFrame(mainroop);
+}
+requestAnimationFrame(mainroop);
+
+
+
