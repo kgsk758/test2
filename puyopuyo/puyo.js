00c9c157e5822d228d8ac047cc8036c5035249f3
kgsk758 <yhasutha2@gmail.com>
2025-03-11 09:51:04 +0900

    touch

--- puyopuyo/puyo.js
+++ puyopuyo/puyo.js
@@ -4,6 +4,14 @@ const ctx = canvas.getContext("2d");
 const ROWS = 14;
 const COLUMNS = 6;
 let SIZE = 40;
+let pos = {
+    x: 0,
+    y: 0
+};
+let touchpos = {
+    x: null,
+    y: null
+};
 const width = window.innerWidth;
 console.log(width);
 if(width <= 480){
@@ -16,16 +24,25 @@ canvas.style.height = `${(ROWS-2)*(SIZE)}px`;
 }
 //タッチ処理
 let is_touched = false;
-document.addEventListener("touchstart", ()=>{
-    is_touched = true;
-});
-document.addEventListener("touchend", ()=>{
-    is_touched = false;
-});
+document.addEventListener("touchstart", (event)=>{
+    let touch = event.touches[0];
+    touchpos.x = touch.clientX;
+    touchpos.y = touch.clientY;
+})
+document.addEventListener("touchmove", (event)=>{
+    let touch = event.touches[0];
+    touchpos.x = touch.clientX;
+    touchpos.y = touch.clientY;
+})
+document.addEventListener("touchend", (event)=>{
+    let touch = event.touches[0];
+    touchpos.x = null;
+    touchpos.y = null;
+})
 
 //ずっと処理
 function mainroop(){
-    console.log(is_touched);
+    console.log(touchpos);
     requestAnimationFrame(mainroop);
 }
 requestAnimationFrame(mainroop);
